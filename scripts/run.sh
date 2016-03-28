c9a2c4acf4a38daf526fa8539045954ef8172e43
Aamir Ahmad <aamirmushtaq1@gmail.com>
2016-03-28 17:42:38 +0530

    Added dir

---
+++ scripts/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+export SONARQUBE_JDBC_URL=jdbc:mysql://$MYSQL_HOST:$MYSQL_PORT/sonar?useUnicode=true\&amp\;characterEncoding\=utf8
+export SONARQUBE_JDBC_PASSWORD=$MYSQL_PASSWORD
+if [ "${1:0:1}" != '-' ]; then
+        exec "$@"
+	fi
+
+	exec java -jar lib/sonar-application-$SONAR_VERSION.jar \
+	  -Dsonar.log.console=true \
+	    -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
+	      -Dsonar.jdbc.password="$SONARQUBE_JDBC_PASSWORD" \
+	        -Dsonar.jdbc.url="$SONARQUBE_JDBC_URL" \
+		  -Dsonar.web.host="0.0.0.0" \
+		    -Dsonar.web.port="8080" \
+		      -Dsonar.web.javaAdditionalOpts="-Djava.security.egd=file:/dev/./urandom" \
+		              "$@"
+
